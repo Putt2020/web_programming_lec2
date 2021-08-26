15d6eabcea5b46aaa52550d987490f19fe61dad8
Putt2020 <6230067421@student.chula.ac.th>
2021-08-26 21:51:33 +0700

    finish the ex2 params input and error handling

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,7 +5,7 @@ class MainController < ApplicationController
   def xxx
   	ya = params[:a]
   	yb = params[:b]
-  	@q = params['_dde353']
+  	@q = params[:_dde353]
   	@result = ya.to_i + yb.to_i
   end
   
@@ -16,7 +16,15 @@ class MainController < ApplicationController
   end
   
   def ex2
-  	inputA = params['a']
-  	inputB = params['b']
+  	@satisfy = false
+  	if (params[:a].presence && params[:b].presence)
+  		inputA = params[:a]
+	  	inputB = params[:b]
+  		if (params[:a].to_i.to_s == params[:a] && params[:b].to_i.to_s == params[:b])
+	  		@satisfy = true
+	  		@a_int = inputA.to_i
+	  		@b_int = inputB.to_i
+	  	end
+	end	
   end
 end
